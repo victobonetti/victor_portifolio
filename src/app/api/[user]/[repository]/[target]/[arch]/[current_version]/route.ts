03b125fda5a2a4eb0eb69b4d041c45727f083b99
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-07-18 15:09:45 -0300

    Updating signature

--- src/app/api/[user]/[repository]/[target]/[arch]/[current_version]/route.ts
+++ src/app/api/[user]/[repository]/[target]/[arch]/[current_version]/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import getBundlesForTarget from "./(getBundles)/getBundles";
+import axios from "axios";
+import Bundle from "./(getBundles)/bundleClass";
 
 export async function GET(req: Request, { params }: { params: { user: string, repository: string, target: string, arch: string, current_version: string } }) {
 
@@ -10,15 +12,22 @@ export async function GET(req: Request, { params }: { params: { user: string, re
     let params_user = params.user;
     let params_repository = params.repository;
 
-    let res_bundle = await getBundlesForTarget({target :params_target, repository: params_repository, user: params_user, current_version: params_current_version});
+    let res_bundle: Bundle | void = await getBundlesForTarget({ target: params_target, repository: params_repository, user: params_user, current_version: params_current_version });
 
     if (res_bundle && res_bundle.signature && res_bundle.url) {
-        return NextResponse.json({
-           version: res_bundle.version,
-           url: res_bundle.url,
-           signature: res_bundle.signature,
-           notes: res_bundle.notes
-        })
+        try {
+            let convertedSignature = await axios.get(res_bundle.signature);
+            res_bundle.signature = convertedSignature.data;
+
+            return NextResponse.json({
+                version: res_bundle.version,
+                url: res_bundle.url,
+                signature: res_bundle.signature,
+                notes: res_bundle.notes
+            })
+        } catch (e) {
+            return NextResponse.json({e})
+        }
     } else {
         return NextResponse.json({})
     }
