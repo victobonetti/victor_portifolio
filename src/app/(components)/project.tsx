e65a9904d327fc958d176e69ad73ee0f901124bc
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-07-19 16:10:10 -0300

    updating button

--- src/app/(components)/project.tsx
+++ src/app/(components)/project.tsx
@@ -104,7 +104,7 @@ export default async function PortifolioProject(props: portifolioProps) {
                         }
                         {downloadData &&
                             <details className="  mt-8 md:mt-0 cursor-pointer ">
-                                <summary className=" btn dark:bg-slate-300 dark:text-slate-800 text-slate-100 bg-slate-900 hover:bg-slate-950 focus:outline-none font-medium text-sm px-5 py-2.5">Downloads</summary>
+                                <summary className=" btn dark:bg-slate-300 dark:text-slate-800 text-slate-100 bg-slate-900 dark:hover:bg-slate-400 hover:bg-slate-950 focus:outline-none font-medium text-sm px-5 py-2.5">Downloads</summary>
                                 <ul className="  bg-slate-50 z-30 shadow rounded-box w-36">
                                     <Link href={String(downloadData?.windows)}><li className="flex items-center p-1 hover:bg-slate-200"><span><FontAwesomeIcon className="text-slate-600 h-6 mr-2" icon={faDownload} /></span><p className="text-slate-700">Windows</p></li></Link>
                                     <Link href={String(downloadData?.darwin)}><li className="flex items-center p-1 hover:bg-slate-200"><span><FontAwesomeIcon className="text-slate-600 h-6 mr-2" icon={faDownload} /></span><p className="text-slate-700">MacOs</p>  </li></Link>
