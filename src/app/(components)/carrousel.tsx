acec541c2e67e84d405ce30f3eae36ff4ab8a654
victobonetti <victorbonettixd@gmail.com>
2023-08-06 20:56:47 -0300

    carrousel fix

--- src/app/(components)/carrousel.tsx
+++ src/app/(components)/carrousel.tsx
@@ -30,21 +30,24 @@ export default function Carrousel({ images }: { images: StaticImageData[] }) {
 
     return (
         < div className=' h-full relative w-full md:w-1/2 items-center ' >
-            <div className=' h-full overflow-hidden'>
+            <div className=' h-full relative'>
                 {
                     images?.map((img, i) => {
                         return (<Image key={i} className={`${active == i ? " block " : " hidden "} h-full object-fit enterAnimation object-left-top mb-4 md:mb-0 `} src={img} alt="Plano de fundo" />)
                     })
+
+
                 }
 
                 {
                     images.length > 1 &&
 
-                    <div className=" absolute flex items-center justify-between">
-                        <div onClick={() => decrement()} className=' relative z-100 shadow rounded-full bg-slate-800 border border-slate-800 cursor-pointer select-none hover:scale-105 transition-all text-slate-200'>  <FontAwesomeIcon className="text-2xl" icon={faArrowCircleLeft} /></div>
-                        <div onClick={() => increment()} className=' relative z-100 shadow rounded-full bg-slate-800 border border-slate-800 cursor-pointer select-none hover:scale-105 transition-all  text-slate-200'><FontAwesomeIcon className="text-2xl" icon={faArrowCircleRight} /></div>
+                    <div className=" bg-red-400 h-full w-full absolute top-0 flex items-center justify-between">
+                        <div onClick={() => decrement()} className=' z-100 shadow rounded-full bg-slate-800 border border-slate-800 cursor-pointer select-none hover:scale-105 transition-all text-slate-200'>  <FontAwesomeIcon className="text-2xl" icon={faArrowCircleLeft} /></div>
+                        <div onClick={() => increment()} className=' z-100 shadow rounded-full bg-slate-800 border border-slate-800 cursor-pointer select-none hover:scale-105 transition-all  text-slate-200'><FontAwesomeIcon className="text-2xl" icon={faArrowCircleRight} /></div>
                     </div>
                 }
+
             </div>
 
         </div >
